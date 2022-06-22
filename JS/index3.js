2be7835dc8f3d603526088c43a778e6ff9f447e5
Paras Raba <parasgadhvi12345@gmail.com>
2022-06-22 15:39:01 +0530

    Removed the bug where the validation massage was displayed even after disabling the checkbox

--- JS/index3.js
+++ JS/index3.js
@@ -87,6 +87,7 @@ $(document).on('click','.some input[type=checkbox]',function(event){
         $('.days input[type=checkbox]').attr('disabled',true);
         $('.time select').val('').change();
         $('.time select').attr('disabled',true);
+        $(this).parent().parent().parent().siblings('.Week').children().children('.time').children('.error').closest('label').hide();
         
     }
     
@@ -101,7 +102,9 @@ $(document).on('click','.days input[type=checkbox]',function(event){
     else{
         // $(this).siblings('.time').closest('select').attr('disabled',true);
         $(this).parent().parent().siblings('.time').children().val('').change();
-        $(this).parent().parent().siblings('.time').children().attr('disabled',true)
+        $(this).parent().parent().siblings('.time').children().attr('disabled',true);
+        $(this).parent().parent().siblings('.time').children('.error').closest('label').hide();
+
     }
 });
 
@@ -110,7 +113,13 @@ $('#myForm').submit(function(event){
     localStorage.clear()
     // console.log($(this).children('.Week').children().children('.time').children().is(':disabled'));
 
-    var flag = true;
+    // first we need to confirm if any of the days checkboxes has been checked or not
+    if($('.days input[type=checkbox]').is(':checked')){
+        var flag = true;    // if it is checked then only we want to proceed
+    }
+    else{
+        var flag = false;   // othrwise we do not want to procedd
+    }
     for(let i=1;i<8;i++){
         if($('#pickupTime'+i).parent().parent().children('.days').children().children().is(':checked') && $('#pickupTime'+i).val() === ''){
             // if the days checkbox is checked and the value of pickupTime is null then  flag will be set at false
@@ -124,6 +133,7 @@ $('#myForm').submit(function(event){
        $('#email').valid() &&
        $('#phone').valid() &&
        $("input[name='gender']:checked").val() !== undefined &&
+       $("#config").valid() &&
        flag
        )
     {
